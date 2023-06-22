28001617acfd024d066dc9979a471163afa5f618
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2023-06-22 19:22:33 +0300

    add schema for clients and products

--- db.js
+++ db.js
@@ -1,17 +1,51 @@
 const mongo=require('mongoose');//need to call the DB
-const url="mongodb://localhost:27017/surfDB"//calling the wanted DB
+const url="mongodb://0.0.0.0:27017/surfDB"//calling the wanted DB
 console.log("hey");
  //await mongoose.connect("mongodb://localhost:27017/surfDB");
-mongo.connect("mongodb://localhost:27017",{usedNewUrlParser:true},(err)=>{
-    if (!err){console.log('MongoDB connection succeeded')}
-    else{console.log('Error connection MongoDB:'+err)}
+mongo.connect(url,(err)=>{
+    if (!err)
+    console.log('MongoDB connection succeeded');
+    else
+    console.log('Error connection MongoDB:'+err);
 
 });
 
 
-//mongo.use(express.urlencoded({extended:false}));
+////////COLLECTION STRUCTURE//////////////////////
 
+/////////////////CLIENT//////////////
+const clientStructor= new mongo.Schema({
+    name: String,
+    email:String,
+    password: Number
+});
 
+const Client=mongo.model("client",clientStructor);
+
+const client = new Client({
+    name:"Idan Barhom",
+    email: "Idan.barhom@gmail.com",
+    password: 12345
+});
+
+//client.save();
+
+///////////PRODUCT///////////////
+const productStructor= new mongo.Schema({
+    id: Number,
+    type:String,
+    price: Number
+});
+
+const Product=mongo.model("product",productStructor);
+
+const product = new Product({
+    id: 2,
+    type: "Bikini",
+    price: 45.99
+});
+
+product.save();
 
 
 
