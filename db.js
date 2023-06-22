837f02410c1e6c86fb51087a500446567d366353
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2023-06-22 20:10:53 +0300

    add order Schema, product Schema, and add requiments

--- db.js
+++ db.js
@@ -15,37 +15,84 @@ mongo.connect(url,(err)=>{
 
 /////////////////CLIENT//////////////
 const clientStructor= new mongo.Schema({
-    name: String,
-    email:String,
-    password: Number
+    name:{
+
+        type:String,
+        required:[true,"Please add name"]
+    },
+    email:
+    { type:String,
+        required:[true,"Please add email"] },
+    password:
+    { type:Number,
+       min:5,
+       max:10 
+    },
+    cart: CartSchema,
+    order: OrderSchema
 });
 
-const Client=mongo.model("client",clientStructor);
+const Client=mongo.model("client",clientStructor);//naming the collection "client"
 
-const client = new Client({
-    name:"Idan Barhom",
-    email: "Idan.barhom@gmail.com",
-    password: 12345
-});
+// const client = new Client({
+//     name:"Idan Barhom",
+//     email: "Idan.barhom@gmail.com",
+//     password: 12345
+// });
 
 //client.save();
 
 ///////////PRODUCT///////////////
 const productStructor= new mongo.Schema({
-    id: Number,
-    type:String,
-    price: Number
+    id:{type: Number,
+        min:1,
+        max:10},
+    type:{type:String,
+        required:[true]},
+    price:{type: Number,
+        min:1}
 });
 
-const Product=mongo.model("product",productStructor);
+const Product=mongo.model("product",productStructor);//naming the collection "product"
 
-const product = new Product({
-    id: 2,
-    type: "Bikini",
-    price: 45.99
+//testing
+
+// const product = new Product({
+//     id: 2,
+//     type: "Bikini",
+//     price: 45.99
+// });
+////////////////////CART////////////////
+const CartStructor= new mongo.Schema({
+    product:{type:productStructor,
+        required:[true]}
 });
 
-product.save();
+const Cart=mongo.model("cart",productStructor);//naming the collection "product"
+
+const cart = new Cart({
+    product:productStructor
+});
+///////////////////ORDERS////////////////////////
+const OrderStructor= new mongo.Schema({
+    orderID:{type: Number,
+        min:1,
+        max:10},
+    price:{type: Number,
+        min:1},
+        items:{type:CartStructor,
+            required:[true]}
+        
+});
+
+const Order=mongo.model("order",productStructor);//naming the collection "product"
+
+const order = new Order({
+    orderID:2,
+    price:24.99,
+    //items: cart     /// cart collection
+
+});
 
 
 
