dba7b21b27779a1e5de6cc27f3bd051a64748999
tal-avni <tal.avni10@gmail.com>
2023-06-23 15:32:29 +0300

    Removed duplicated files

---
+++ main.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express=require("express");
+const app=express();
+
+app.get("/",function(req, res){
+  //console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res){
+  res.send("thanks for posting that");
+
+
+});
+
+app.listen(3000,function(){
+  console.log("server started on port 3000");
+});
+
