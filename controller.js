c717bc4b80a46375969c63c69533b4b5913985ef
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2023-06-26 12:06:39 +0300

    db

--- controller.js
+++ controller.js
@@ -1,14 +1,33 @@
 //jshint esversion:6
 //creating a server
-const express = require('express');
-const bodyparser=require('body-parser');
-const app = express();
-const http = require('http');//taking data from the web. 
-const server = http.createServer(app);//creating the server web
-const myDB = require("./db");//a way to reach to db.js
+let express = require('express');
+let bodyparser=require('body-parser');
+let app = express();
+let http = require('http');//taking data from the web. 
+let server = http.createServer(app);//creating the server web
+let myDB = require("./db");//a way to reach to db.js
 //const Client = require("./db").Client
-const io = require('socket.io')(server);//connecting between the server and the client
-server.listen(3000);
+let io = require('socket.io')(server);//connecting between the server and the client
+server.listen(8080);
+
+io.sockets.on('connection',function(socket){//new client connection
+  console.log(socket.id);
+
+  socket.on('register',function(name,email,pw) {
+     let valid=myDB.addClient(name,email,pw);
+
+    if(valid===1){
+      socket.emit('success-register',name);
+    }
+    else
+      socket.emit('failed-register');
+
+  })
+});
+
+
+
+
 
 //Routes
 app.use('/', express.static("pages"));
@@ -51,23 +70,24 @@ app.get("/style",function(req, res){
 });
 
 
- 
+ app.listen(3000,function(){
+   console.log("Server is running on port 3000");
+ });
 
 
 
 
 
- const { addClient } = require("./db");
- app.post("/register",function(req,res){
+//  const { addClient } = require("./db");
+//  app.post("/register",function(req,res){
 
-  const name=req.body.name;
-  const email=req.body.email;
-  const password=req.body.password;
-  addClient(name,email,password);
+//   const name=req.body.name;
+//   const email=req.body.email;
+//   const password=req.body.password;
+//   addClient(name,email,password);
+//   res.redirect("/");
   
-  res.redirect("/");
-  
- })
+//  })
 // function addClient(Name,Email,Password){
 //   let instert= new Client({
 //     name:Name,
@@ -76,5 +96,3 @@ app.get("/style",function(req, res){
 //   })
 //   console.log("success");
 // }
-
-
