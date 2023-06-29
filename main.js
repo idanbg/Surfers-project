103b927d907c404c9cac585699711357ca32c513
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2023-06-29 17:39:33 +0300

    somthing

--- main.js
+++ main.js
@@ -1,42 +1,75 @@
-const express=require('express');
-const app=express();
-const bodyParser=require('body-parser');
-const morgan=require('morgan');
-const mongo=require('mongoose');//need to call the DB
-const url="mongodb+srv://Golshim123:Golshim123@webdevproject.k2obzer.mongodb.net/surfDB"//calling the wanted DB
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const url = "mongodb+srv://Golshim123:Golshim123@webdevproject.k2obzer.mongodb.net/surfDB";
+// Serve static files from the 'public' folder
+app.use(express.static('views'));
 
-//middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
+const productsRouts = require('./backend/routs/products');
+const accountsRouts = require('./backend/routs/accounts');
+const ordersRouts = require('./backend/routs/orders');
+const orderItemsRouts=require('./backend/routs/orderitems')
 
+app.use("/products", productsRouts);
+app.use("/accounts", accountsRouts);
+app.use("/orders", ordersRouts);
+app.use("/orderitems", orderItemsRouts);
 
-
-const productsRouts=require('./backend/routs/products')
-const accountsRouts=require('./backend/routs/accounts')
-const ordersRouts=require('./backend/routs/orders')
-const orderitemsRouts=require('./backend/routs/orderitems')
-
-
-app.use("/products",productsRouts);
-app.use("/accounts",accountsRouts);
-app.use("/orders",ordersRouts);
-app.use("/ordersitems",orderitemsRouts);
-
-
-
-mongo.connect(url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    dbName:'surfDB'
-})
-.then(()=>{
-    console.log('DB Connection Success!')
-})
-.catch((err)=>{
+// MongoDB Connection
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('DB Connection Success!');
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
+
+
+  app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/homePage.html');
+  });
+  
+  app.get("/shop/women", function (req, res) {
+    res.sendFile(__dirname + '/views/women.html');
+  });
+  
+  app.get("/shop/men", function (req, res) {
+    res.sendFile(__dirname + '/views/men.html');
+  });
+  
+  app.get("/shop", function (req, res) {
+    res.sendFile(__dirname + '/views/shop.html');
+  });
+  
+  app.get("/logIn", function (req, res) {
+    res.sendFile(__dirname + '/views/logIn.html');
+  });
+  
+  app.get("/logIn", function (req, res) {
+    res.sendFile(__dirname + '/views/logIn.css');
+  });
+  
+  app.get("/register", function (req, res) {
+    res.sendFile(__dirname + '/views/register.html');
+  });
+  
+  app.get("/register", function (req, res) {
+    res.sendFile(__dirname + '/views/logIn.css');
+  });
+  
+  app.get("/style", function (req, res) {
+    res.sendFile(__dirname + '/views/style.css');
+  });
+  
 
 
 
@@ -48,6 +81,24 @@ mongo.connect(url,{
 app.listen(3400,()=>{
     console.log("server is running http://localhost3400");
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //with "await" when we want to find an object we first calling it , and its going to "wait" until its finding the object, and only then going to print it, we need to add the "async" in the beginning of the function.
