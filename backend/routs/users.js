dd6aa6ad8bfa297ed96da0af35d1494e59673f8f
Idan Barhom <130668498+IdanBarhom@users.noreply.github.com>
2023-06-30 12:54:20 +0300

    almost finishing loging in and register

--- backend/routs/users.js
+++ backend/routs/users.js
@@ -1,17 +1,31 @@
 const User=require('../models/user');
 const express=require('express')
 const router=express.Router();
+const bcrypt=require('bcrypt')
 
 console.log("bye")
+
+
 router.get('/',async(req,res)=>{
 
-    const user=await User.find();
+    const user=await User.find()
 
     if(!user){
         res.status(500).json({success:false})
     }
     res.send(user);
 }) 
+
+router.get('/id',async(req,res)=>{
+
+    const user=await User.find.findById(req.params.id);
+
+    if(!user){
+        res.status(500).json({message:'There is no such user with the given ID'});
+    }
+    res.send(user);
+}) 
+
 router.post('/',async(req,res)=>{
      let user = new User({
         name: req.body.name,
@@ -19,7 +33,8 @@ router.post('/',async(req,res)=>{
         password:req.body.password,
         city:req.body.city,
         street:req.body.street,
-        streetNum: req.body.streetNum
+        streetNum: req.body.streetNum,
+        permission:req.body.permission
     })
     user =await user.save();
     if (!user)
@@ -28,6 +43,14 @@ router.post('/',async(req,res)=>{
     res.send(user);
 })
 
+
+router.post('/login',async(req,res)=>{
+    const user = await User.findOne({name:req.body.name})
+    if(!user)
+    return res.status(400).send('The user not found')
+
+    return res.status(200).send(user);
+})
 
 
 module.exports=router;
